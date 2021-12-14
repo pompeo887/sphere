5185281c424491ff5e318cdc3fd8f2ea00c0cf09
pompeo t <pmp.tanga@gmail.com>
2021-12-14 15:26:37 +0100

    hh

---
+++ data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2021-12-14-at-125350",
+      "name": "WhatsApp Image 2021-12-14 at 12.53.50",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.5593986672858922,
+        "pitch": 0.47153865541933726,
+        "fov": 1.4108347438219848
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
